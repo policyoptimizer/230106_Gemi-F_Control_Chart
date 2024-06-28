dac7ac59be791df9957d7174c0402c9d7d8ef5ec
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-06-28 17:29:19 +0900

    Update rev.03.py

--- Dash/by product_DP58/rev.03.py
+++ Dash/by product_DP58/rev.03.py
@@ -178,7 +178,7 @@ def create_dp58_tabs(window_size, sigma_level, recent_batches, trend_threshold):
 
 # 앱 레이아웃 설정
 app.layout = html.Div([
-    html.H1('DP58 Analysis Dashboard'),
+    html.H1('CQA DP58 Assay, Chiral'),
     dcc.Tabs(id="dp58-tabs", children=create_dp58_tabs(5, 1, 5, 0.02)),
     html.Div([
         html.H3('넬슨 법칙'),
