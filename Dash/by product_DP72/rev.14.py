b0caff215fe7906c4ff2d9cffb987ba8b67d4dfe
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-04 10:28:35 +0900

    Update rev.14.py

--- Dash/by product_DP72/rev.14.py
+++ Dash/by product_DP72/rev.14.py
@@ -1,7 +1,9 @@
 # 시험규격 추가
-# DP72 Assay : 
-# DP72 Chiral : 
-# DP72 AUI :  
-# DP72 Total_Impurity : 
-# DP72 ROI : 
-# DP72 Impurity-1 : 
+# DP72 Assay : 98.0~102.0%
+# DP72 Chiral : 99.0%ee 이상
+# DP72 AUI :  0.1% 이하
+# DP72 Total_Impurity : 2.0% 이하
+# DP72 ROI : 0.20% w/w 이하
+# DP72 Impurity-1 : 1.0% 이하
+
+
