06dfddfb4871f5120f3fe3d7bd80b0b3f0c711e1
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-10 15:18:39 +0900

    d

--- Dash_chemverse/전처리/product_rev.03.py
+++ Dash_chemverse/전처리/product_rev.03.py
@@ -1,1 +1,20 @@
-ff
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+tb_insp_result_sample = dataiku.Dataset("tb_insp_result_sample")
+tb_insp_result_sample_df = tb_insp_result_sample.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+data_rev03_df = tb_insp_result_sample_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+data_rev03 = dataiku.Dataset("data_rev03")
+data_rev03.write_with_schema(data_rev03_df)
