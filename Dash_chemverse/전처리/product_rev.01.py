a36fa34e0c91281db78de46e30b6958e4e47bc83
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-11 14:44:16 +0900

    Update product_rev.01.py

--- Dash_chemverse/전처리/product_rev.01.py
+++ Dash_chemverse/전처리/product_rev.01.py
@@ -14,16 +14,12 @@ data_rev03_df = data_rev03.get_dataframe()
 # TODO: Write here your actual code that computes the outputs
 # NB: DSS supports several kinds of APIs for reading and writing data. Please see doc.
 
-DP14Chunbo_df = ... # Compute a Pandas dataframe to write into DP14Chunbo
-DP14Langhua_df = ... # Compute a Pandas dataframe to write into DP14Langhua
-DP18_c_df = ... # Compute a Pandas dataframe to write into DP18_c
 DP26_c_df = ... # Compute a Pandas dataframe to write into DP26_c
 DP26onsan_df = ... # Compute a Pandas dataframe to write into DP26onsan
 DP37_c_df = ... # Compute a Pandas dataframe to write into DP37_c
 DP57_c_df = 
 DP58_c_df = 
 DP58onsan_df = 
-DP60_df = 
 DP67_c_df = 
 DP72_c_df = 
 Zemiglo_df = 
@@ -40,12 +36,6 @@ Zemimet_25_750mg_df =
 
 
 # Write recipe outputs
-DP14Chunbo = dataiku.Dataset("DP14Chunbo")
-DP14Chunbo.write_with_schema(DP14Chunbo_df)
-DP14Langhua = dataiku.Dataset("DP14Langhua")
-DP14Langhua.write_with_schema(DP14Langhua_df)
-DP18_c = dataiku.Dataset("DP18_c")
-DP18_c.write_with_schema(DP18_c_df)
 DP26_c = dataiku.Dataset("DP26_c")
 DP26_c.write_with_schema(DP26_c_df)
 DP26onsan = dataiku.Dataset("DP26onsan")
@@ -58,8 +48,6 @@ DP58_c = dataiku.Dataset("DP58_c")
 DP58_c.write_with_schema(DP58_c_df)
 DP58onsan = dataiku.Dataset("DP58onsan")
 DP58onsan.write_with_schema(DP58onsan_df)
-DP60 = dataiku.Dataset("DP60")
-DP60.write_with_schema(DP60_df)
 DP67_c = dataiku.Dataset("DP67_c")
 DP67_c.write_with_schema(DP67_c_df)
 DP72_c = dataiku.Dataset("DP72_c")
