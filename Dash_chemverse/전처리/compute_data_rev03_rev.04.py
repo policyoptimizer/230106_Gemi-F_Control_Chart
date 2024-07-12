34a56b4e62f5fff82d14386d2a3b8fbaee3eaf3d
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-12 12:53:24 +0900

    Update compute_data_rev03_rev.04.py

--- Dash_chemverse/전처리/compute_data_rev03_rev.04.py
+++ Dash_chemverse/전처리/compute_data_rev03_rev.04.py
@@ -32,6 +32,7 @@
 # 'Zemilow 50/10mg'
 # 'Zemilow 50/20mg'
 
+
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd
@@ -46,58 +47,56 @@ tb_insp_result_sample_df = tb_insp_result_sample.get_dataframe()
 columns_to_keep = ['batch_no', 'insp_code', 'insp_min_value', 'insp_max_value', 'insp_result_value']
 filtered_df = tb_insp_result_sample_df[columns_to_keep]
 
-# batch_no와 insp_code를 문자열로 변환
-filtered_df['batch_no'] = filtered_df['batch_no'].astype(str)
-filtered_df['insp_code'] = filtered_df['insp_code'].astype(str)
+# batch_no와 insp_code를 문자열로 변환하고 공백 제거
+filtered_df['batch_no'] = filtered_df['batch_no'].astype(str).str.strip()
+filtered_df['insp_code'] = filtered_df['insp_code'].astype(str).str.strip()
 
 # product 및 site 컬럼 추가 함수 정의
 def add_product_and_site(row):
     batch_no = row['batch_no']
     insp_code = row['insp_code']
    
-    if isinstance(batch_no, str):
-        if batch_no.startswith('HFP'):
-            return 'DP26 onsan', 'Onsan'
-        elif batch_no.startswith('NFP'):
-            return 'DP37', 'Onsan'
-        elif batch_no.startswith('DPF'):
-            return 'DP57', 'Onsan'
-        elif batch_no.startswith('BAN'):
-            return 'DP58 onsan', 'Onsan'
-        elif batch_no.startswith('DPY'):
-            return 'DP67', 'Onsan'
-        elif batch_no.startswith('DPT'):
-            return 'DP67', 'Iksan'
-        elif batch_no.startswith('DPS'):
-            return 'DP72', 'Onsan'
-        elif batch_no.startswith('DPB'):
-            return 'DP72', 'Iksan'
-        elif batch_no.startswith('GEM'):
-            return 'Zemiglo', 'Osong'
-        elif batch_no.startswith('GLA'):
-            return 'Zemidapa', 'Osong'
-        elif batch_no.startswith('GMT'):
-            return 'Zemimet 50/500mg', 'Osong'
-        elif batch_no.startswith('ZMG'):
-            return 'Zemimet 50/1000mg', 'Osong'
-        elif batch_no.startswith('ZMJ'):
-            return 'Zemimet 25/500mg', 'Osong'
-        elif batch_no.startswith('ZMK'):
-            return 'Zemimet 25/1000mg', 'Osong'
-        elif batch_no.startswith('ZRA'):
-            return 'Zemilow 50/5mg', 'Osong'
-        elif batch_no.startswith('ZRB'):
-            return 'Zemilow 50/10mg', 'Osong'
-        elif batch_no.startswith('ZRC'):
-            return 'Zemilow 50/20mg', 'Osong'
-        elif batch_no.startswith('ZML'):
-            return 'Zemimet 25/750mg', 'Osong'
+    if batch_no.startswith('HFP'):
+        return 'DP26 onsan', 'Onsan'
+    elif batch_no.startswith('NFP'):
+        return 'DP37', 'Onsan'
+    elif batch_no.startswith('DPF'):
+        return 'DP57', 'Onsan'
+    elif batch_no.startswith('BAN'):
+        return 'DP58 onsan', 'Onsan'
+    elif batch_no.startswith('DPY'):
+        return 'DP67', 'Onsan'
+    elif batch_no.startswith('DPT'):
+        return 'DP67', 'Iksan'
+    elif batch_no.startswith('DPS'):
+        return 'DP72', 'Onsan'
+    elif batch_no.startswith('DPB'):
+        return 'DP72', 'Iksan'
+    elif batch_no.startswith('GEM'):
+        return 'Zemiglo', 'Osong'
+    elif batch_no.startswith('GLA'):
+        return 'Zemidapa', 'Osong'
+    elif batch_no.startswith('GMT'):
+        return 'Zemimet 50/500mg', 'Osong'
+    elif batch_no.startswith('ZMG'):
+        return 'Zemimet 50/1000mg', 'Osong'
+    elif batch_no.startswith('ZMJ'):
+        return 'Zemimet 25/500mg', 'Osong'
+    elif batch_no.startswith('ZMK'):
+        return 'Zemimet 25/1000mg', 'Osong'
+    elif batch_no.startswith('ZRA'):
+        return 'Zemilow 50/5mg', 'Osong'
+    elif batch_no.startswith('ZRB'):
+        return 'Zemilow 50/10mg', 'Osong'
+    elif batch_no.startswith('ZRC'):
+        return 'Zemilow 50/20mg', 'Osong'
+    elif batch_no.startswith('ZML'):
+        return 'Zemimet 25/750mg', 'Osong'
    
-    if isinstance(insp_code, str):
-        if insp_code.startswith('106113'):
-            return 'DP26', 'Onsan'
-        elif insp_code.startswith('106114'):
-            return 'DP58', 'Onsan'
+    if insp_code.startswith('106113'):
+        return 'DP26', 'Onsan'
+    elif insp_code.startswith('106114'):
+        return 'DP58', 'Onsan'
    
     return np.nan, np.nan
 
