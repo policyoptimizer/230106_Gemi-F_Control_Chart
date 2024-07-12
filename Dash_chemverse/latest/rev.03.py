304d1afa3d990921b7dbabf79bff7c9847ecb7e5
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-12 16:10:40 +0900

    Update rev.03.py

--- Dash_chemverse/latest/rev.03.py
+++ Dash_chemverse/latest/rev.03.py
@@ -1,9 +1,8 @@
-연도를 18 ~ 26 으로 수정
-현재 20 ~ 24 로 되어 있음
+DP67, DP72 제품에서 iksan, onsan 선택하는 버튼은 확인했는데 클릭해도 iksan, onsan 분류가 안되요
+DP67과 DP72 제품에 대해 Iksan과 Onsan 버튼 클릭 시 데이터가 올바르게 필터링되도록 수정
 
-상단 제품 탭을 2개의 열로 구분해달라고 했는데 실제로는 한 줄입니다.
-지금 보니 그것도 나쁘지 않으니까 그냥 놔두세요
-다만 아래와 같이 버튼에 쓰인 내용이 3줄로 되어 있는데요
+상단 제품 탭의 글자 사이즈를 작게 조정해주세요
+아래와 같이 버튼에 쓰인 내용이 3줄로 되어 있는데요
 Zemilow
 50
 20mg
@@ -11,9 +10,6 @@ Zemilow
 글자로 좀 작게 하든지 해서 아래와 같이 상단 제품 탭이 되도록 해주세요
 Zemilow
 50/20mg
-
-DP67, DP72 제품에서 iksan, onsan 선택하는 버튼은 확인했는데 클릭해도 iksan, onsan 분류가 안되요
-DP67과 DP72 제품에 대해 Iksan과 Onsan 버튼 클릭 시 데이터가 올바르게 필터링되도록 수정
 
 화면에서 보여질 때
 칼럼 순서를 batch_no / product / site / insp_code / insp_min_value / insp_max_value/ insp_result_value 이런 순서로 구성해주세요
