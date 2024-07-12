8da912e9ef33ba88c13ee168d94dedc97964fa3a
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-12 10:14:15 +0900

    Update compute_data_rev04_rev.01.py

--- Dash_chemverse/전처리/compute_data_rev04_rev.01.py
+++ Dash_chemverse/전처리/compute_data_rev04_rev.01.py
@@ -9,7 +9,7 @@ product : DP72
 DPB-정량01 > Assay
 DPB-키랄순도01 > Chiral
 DPB-순도(총불순물)01 > Total Impurity
-DPB-순도(DP-IMP-1)01 > Impurity-1
+DPB-순도(DP-IMP-1)01 > IMP-1
 DPB-순도(미특정불순물)01 > AUI
 DPB-강열잔분01 > ROI
 
@@ -17,7 +17,7 @@ site : Iksan
 product : DP67
 
 DPT-순도(총불순물)01 > Total Impurity
-DPT-순도(DP-IMP-1)01 > Impurity-1
+DPT-순도(DP-IMP-1)01 > IMP-1
 DPT-순도(미특정불순물)01 > AUI
 
 site : Onsan
@@ -26,16 +26,16 @@ product : DP72
 DPS-함량01 > Assay
 DPS-Chiral순도01 > Chiral
 DPS-총불순물합01 > Total Impurity
-DPS-DP-IMP-101 > Impurity-1
-DPS-DPIMP101 > Impurity-1
-DPS-개별불순물01 > Impurity-1
+DPS-DP-IMP-101 > IMP-1
+DPS-DPIMP101 > IMP-1
+DPS-개별불순물01 > AUI
 DPS-강열잔분01 > ROI
 
 site : Onsan
 product : DP67
 
 DPY-유기부성분총합01 > Total Impurity
-DPY-부성분(DP-IMP-1)01 > AUI
+DPY-부성분(DP-IMP-1)01 > IMP-1
 DPY-신규부성분01 > AUI
 
 site : Onsan
@@ -81,7 +81,7 @@ product : Zemiglo
 
 GEM-함량01 > Assay
 GEM-총유연물질01 > Total Impurity
-GEM-DPIMP1유연물질01 > Impurity-1
+GEM-DPIMP1유연물질01 > IMP-1
 GEM-미지개개유연물질01 > AUI
 
 site : Osong
@@ -89,24 +89,24 @@ product : Zemidapa
 
 GLA-제미글립틴함량01 > Assay
 GLA-제미총유연물질01 > Total Impurity
-GLA-제미DPIMP1유연물질01 > Impurity-1
-GLA-제미미지유연물질01 > Impurity-1
+GLA-제미DPIMP1유연물질01 > IMP-1
+GLA-제미미지유연물질01 > AUI
 
 site : Osong
 product : Zemimet 50/500mg
 
 GMT-제미글립틴함량01 > Assay
 GMT-제미총유연물질01 > Total Impurity
-GMT-DPIMP1제미유연물질01 > Impurity-1
-GMT-제미미지유연물질01 > Impurity-1
+GMT-DPIMP1제미유연물질01 > IMP-1
+GMT-제미미지유연물질01 > AUI
 
 site : Osong
 product : Zemimet 50/1000mg
 
 ZMG-제미글립틴함량01 > Assay
 ZMG-총제미유연물질01 > Total Impurity
-ZMG-DPIMP1제미유연물질01 > Impurity-1
-ZMG-미지제미유연물질01 > Impurity-1
+ZMG-DPIMP1제미유연물질01 > IMP-1
+ZMG-미지제미유연물질01 > AUI
 
 site : Osong
 product : Zemimet 25/500mg
@@ -114,49 +114,48 @@ product : Zemimet 25/500mg
 ZMJ-제미글립틴함량01 > Assay
 ZMJ-제미글립틴함량02 > Assay
 ZMJ-제미총유연물질01 > Total Impurity
-ZMJ-DP-IMP-1제미유연물질01 > Impurity-1
-ZMJ-제미미지유연물질01 > Impurity-1
-# ZMJ-DPIMP3제미유연물질01??
+ZMJ-DP-IMP-1제미유연물질01 > IMP-1
+ZMJ-제미미지유연물질01 > AUI
 
 site : Osong
 product : Zemimet 25/1000mg
 
 ZMK-제미글립틴함량01 > Assay
 ZMK-제미총유연물질01 > Total Impurity
-ZMK-제미DPIMP1유연물질01 > Impurity-1
-ZMK-제미미지유연물질01 > Impurity-1
+ZMK-제미DPIMP1유연물질01 > IMP-1
+ZMK-제미미지유연물질01 > AUI
 
 site : Osong
 product : Zemilow 50/5mg
 
 ZRA-제미글립틴함량01 > Assay
 ZRA-총유연물질01 > Total Impurity
-ZRA-DPIMP1제미유연물질01 > Impurity-1
-ZRA-미지유연물질01 > Impurity-1
+ZRA-DPIMP1제미유연물질01 > IMP-1
+ZRA-미지유연물질01 > AUI
 
 site : Osong
 product : Zemilow 50/10mg
 
 ZRB-제미글립틴함량01 > Assay
 ZRB-총유연물질01 > Total Impurity
-ZRB-DPIMP1제미유연물질01 > Impurity-1
-ZRB-미지유연물질01 > Impurity-1
+ZRB-DPIMP1제미유연물질01 > IMP-1
+ZRB-미지유연물질01 > AUI
 
 site : Osong
 product : Zemilow 50/20mg
 
 ZRC-제미글립틴함량01 > Assay
 ZRC-총유연물질01 > Total Impurity
-ZRC-DPIMP1제미유연물질01 > Impurity-1
-ZRC-미지유연물질01 > Impurity-1
+ZRC-DPIMP1제미유연물질01 > IMP-1
+ZRC-미지유연물질01 > AUI
 
 site : Osong
 product : Zemimet 25/750mg
 
 ZML-제미글립틴함량01 > Assay
 ZML-제미총유연물질01 > Total Impurity
-ZML-제미DPIMP1유연물질01 > Impurity-1
-ZML-제미미지유연물질01 > Impurity-1
+ZML-제미DPIMP1유연물질01 > IMP-1
+ZML-제미미지유연물질01 > AUI
 ```
 
 #################################################################
