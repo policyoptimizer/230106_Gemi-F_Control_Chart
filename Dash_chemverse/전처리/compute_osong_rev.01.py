938c7f56b60fcbb4095b0ca1761db384755a5a55
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-15 17:35:59 +0900

    Create compute_osong_rev.01.py

---
+++ Dash_chemverse/전처리/compute_osong_rev.01.py
@@ -0,0 +1,24 @@
+# data_rev04 의 복잡한 데이터 셋을 오송, 익산, 온산 3가지로 나누기
+
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+data_rev04 = dataiku.Dataset("data_rev04")
+data_rev04_df = data_rev04.get_dataframe()
+
+# Compute recipe outputs
+# Osong, Iksan, Onsan 데이터셋으로 나누기
+osong_df = data_rev04_df[data_rev04_df['site'] == '오송']
+iksan_df = data_rev04_df[data_rev04_df['site'] == '익산']
+onsan_df = data_rev04_df[data_rev04_df['site'] == '온산']
+
+# Write recipe outputs
+osong = dataiku.Dataset("osong")
+osong.write_with_schema(osong_df)
+iksan = dataiku.Dataset("iksan")
+iksan.write_with_schema(iksan_df)
+onsan = dataiku.Dataset("onsan")
+onsan.write_with_schema(onsan_df)
