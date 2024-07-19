2439338fa2bfb1ac612309e66ca5ed44b1509f75
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-19 17:30:31 +0900

    Create rev.09.py

---
+++ Dash_chemverse/value chain/rev.09.py
@@ -0,0 +1,71 @@
+# Show/Hide Raw Data
+
+DP58
+DP26
+DP37
+DP57
+DP67
+DP72
+
+제미도 추가
+
+신호등 기준:
+빨강: 시험 규격을 벗어남 (허용기준의 2%를 벗어남)
+초록: 허용기준 내에 있음
+노랑: 허용기준의 2% 이내에서 벗어남
+
+우측 상단에 작게 표기
+좌측 정렬
+
+하단에 메시지
+
+Product: DP14
+Always green.
+
+
+Product: DP58
+Batch: M230407007
+
+
+Product: DP26
+Batch: M230227018
+
+
+Product: DP37
+Batch: NFP23009
+
+
+Product: DP18
+Always green.
+
+
+Product: DP57
+Batch: DPF23024
+
+
+Product: DP60
+Always green.
+
+
+Product: DP67
+Batch: DPT18001
+
+
+Product: DP72
+Batch: DPS24003
+
+
+Product: Gemiglo
+Error: Unable to fetch schema for CDS202404231247_heuiy.Gemiglo_c: b'dataset does not exist: CDS202404231247_heuiy.Gemiglo_c'
+
+
+Product: Gemimet
+Error: Unable to fetch schema for CDS202404231247_heuiy.Gemimet_c: b'dataset does not exist: CDS202404231247_heuiy.Gemimet_c'
+
+
+Product: Gemilow
+Error: Unable to fetch schema for CDS202404231247_heuiy.Gemilow_c: b'dataset does not exist: CDS202404231247_heuiy.Gemilow_c'
+
+
+Product: Gemidapa
+Error: Unable to fetch schema for CDS202404231247_heuiy.Gemidapa_c: b'dataset does not exist: CDS202404231247_heuiy.Gemidapa_c'
