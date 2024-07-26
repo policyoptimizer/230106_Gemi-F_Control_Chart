d5f4882592522f064a57c9b0a6e2fb78b6b38689
policyoptimizer <98368335+policyoptimizer@users.noreply.github.com>
2024-07-26 15:48:00 +0900

    Update compute_DP26_c_rev.02.py

--- Dash_chemverse/전처리/compute_DP26_c_rev.02.py
+++ Dash_chemverse/전처리/compute_DP26_c_rev.02.py
@@ -1,5 +1,72 @@
 # 배치 번호를 오름차순으로 정렬
 
+# 각 제품을
+# batch_no 를 기준으로 오름차순으로 정렬해주세요
+
+# DP26_c
+# 예) Y200106004, M220621012 등
+# Y or M 이후 두 자리는 년도임
+# Y 로 시작하는 배치를 먼저 배치해주고, 그 다음 M 으로 시작하는 배치를 정렬
+# Y or M 으로 시작하지 않는 다른 형식의 배치는 테스트 배치이므로 삭제
+
+# DP26onsan
+# 예) HFP24001
+# HFP로 배치가 시작하고,
+# HFP 이후 2자리는 년도임
+
+# DP37_c
+# 예) NFP20002
+
+# DP57_c
+# 예) DPF18001
+
+# DP58_c
+# 예) Y190614008, M220622003
+# Y 로 시작하는 배치를 먼저 배치해주고, 그 다음 M 으로 시작하는 배치를 정렬
+# Y or M 으로 시작하지 않는 다른 형식의 배치는 테스트 배치이므로 삭제
+
+# DP58onsan
+# 예) BAN24001 형식
+
+# DP67_c
+# 예) DPT15001, DPY23024
+# DPTxxxxx 를 먼저 배치해주고, 그 다음 DPYxxxxx 배치를 정렬
+
+# DP72_c
+# 예) DPB15001
+
+# Zemiglo
+# 예) GEM15740A, GEM17536 등
+# 오름차순으로 정렬
+
+# Zemidapa
+# 예) GLA22501
+
+# Zemimet_50_500mg
+# 예) GMT17544
+
+# Zemimet_50_1000mg
+# 예) ZMG19564A
+
+# Zemimet_25_500mg
+# 예) ZMJ18546
+
+# Zemimet_25_1000mg
+# 예) ZMK19501
+
+# Zemilow_50_5mg
+# 예) ZRA19501
+
+# Zemilow_50_10mg
+# 예) ZRB19501
+
+# Zemilow_50_20mg
+# 예) ZRC20501
+
+# Zemimet_25_750mg
+# 예) ZML23501
+
+
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd
